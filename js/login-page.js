aab71c620be0e75005f603ae2d0a8529db9c98a4
AbubakarKang <90056797+AbubakarKang@users.noreply.github.com>
2022-03-11 18:57:57 +0800

    🎨 Styled body

--- js/login-page.js
+++ js/login-page.js
@@ -1,1 +1,13 @@
-document.body.style.background = "red";
+//----------------------------\\ ELEMENTS //----------------------------\\
+
+const body = document.body;
+
+//----------------------------\\ FUNCTIONS //---------------------------\\
+
+body.style.margin = 0;
+body.style.width = "100vw";
+body.style.height = "100vh";
+body.style.display = "flex";
+body.style.alignItems = "center";
+body.style.flexDirection = "column";
+body.style.justifyContent = "center";
